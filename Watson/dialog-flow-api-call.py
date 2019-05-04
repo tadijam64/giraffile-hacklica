be9d52ed2ea90674dc5b4edf86202aa33fbb73d5
Tadija  Malic <tadijam64@gmail.com>
2019-05-04 20:57:06 +0200

    Script for comminucating with dialogflow chatbot - Returns the result of detect intent with texts as inputs.

---
+++ Watson/dialog-flow-api-call.py
@@ -0,0 +1,43 @@
+import dialogflow
+import argparse
+import os
+
+project_id = "document-search-chatbot-fc080"
+session_id = "test1234"
+texts = "hi boy", "what is up", "how are you"
+language_code = "en"
+
+def detect_intent_texts(project_id, session_id, texts, language_code):
+
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="C:\\Users\\tadija\\Desktop\\project\\Watson\\document-search-chatbot-42134bacfbd1.json"
+
+    """Returns the result of detect intent with texts as inputs.
+
+    Using the same `session_id` between requests allows continuation
+    of the conversation."""
+
+    import dialogflow_v2 as dialogflow
+    session_client = dialogflow.SessionsClient()
+
+    session = session_client.session_path(project_id, session_id)
+    print('Session path: {}\n'.format(session))
+
+    for text in texts:
+        text_input = dialogflow.types.TextInput(
+            text=text, language_code=language_code)
+
+        query_input = dialogflow.types.QueryInput(text=text_input)
+
+        response = session_client.detect_intent(
+            session=session, query_input=query_input)
+
+        print('=' * 20)
+        print('Query text: {}'.format(response.query_result.query_text))
+        print('Detected intent: {} (confidence: {})\n'.format(
+            response.query_result.intent.display_name,
+            response.query_result.intent_detection_confidence))
+        print('Fulfillment text: {}\n'.format(
+            response.query_result.fulfillment_text))
+
+
+detect_intent_texts(project_id, session_id, texts, language_code)
