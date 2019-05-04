e2e4416a434e7891c1a8c1b232e6aa1063728d7c
Leo Siniša Radošić <leo.radosic@gmail.com>
2019-05-04 10:39:22 +0200

    Prvi commit

---
+++ Web/Girafile/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Girafile.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Web/Girafile/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Girafile.WebApiApplication" Language="C#" %>
