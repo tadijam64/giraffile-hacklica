fc98a4bcfef4a8ccff9647ce4b23e91cca72a536
Leo Siniša Radošić <leo.radosic@gmail.com>
2019-05-05 05:17:33 +0200

    [WEB] Dodani login

---
+++ Web/Girafile/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.Owin.Security;
+
+namespace Girafile.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login(string returnUrl)
+        {
+            HttpContext.GetOwinContext().Authentication.Challenge(new AuthenticationProperties
+            {
+                RedirectUri = returnUrl ?? Url.Action("Index", "Home")
+            },
+                "Auth0");
+            return new HttpUnauthorizedResult();
+        }
+    }
+}
