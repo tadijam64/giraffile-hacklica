396f8a9a2813d35b17fb0a19d83068e8f309e878
Leo Siniša Radošić <leo.radosic@gmail.com>
2019-05-05 05:03:43 +0200

    [WEB] Pretrživanje, dodavanje, metadata, spajanje s mikroservisima

---
+++ Web/Girafile/Common/ProcessTools.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+
+namespace Girafile.Common
+{
+    public class ProcessTools
+    {
+        // priprema okruženja za pokretanje python skripti
+        public static System.Diagnostics.ProcessStartInfo pokreniProcesRelativnaPutanja()
+        {
+            return new System.Diagnostics.ProcessStartInfo
+            {
+                WorkingDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
+                WindowStyle = System.Diagnostics.ProcessWindowStyle.Normal,
+                FileName = "cmd.exe",
+                RedirectStandardInput = true,
+                UseShellExecute = false
+            };
+        }
+
+        public static void pokreniProces(string proces, string argumenti)
+        {
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = pokreniProcesRelativnaPutanja();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = proces;
+            startInfo.Arguments = argumenti;
+            process.StartInfo = startInfo;
+            process.Start();
+        }
+    }
+}
---
+++ Web/Girafile/Models/DocumentDTO2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Girafile.Models
+{
+    public class DocumentDTO2
+    {
+        public System.Guid ID { get; set; }
+        public Nullable<System.Guid> IDLanguge { get; set; }
+        public string Name { get; set; }
+        public string MetaData { get; set; }
+        public string Keywords { get; set; }
+        public string MD5 { get; set; }
+        public Nullable<bool> Deleted { get; set; }
+    }
+}
